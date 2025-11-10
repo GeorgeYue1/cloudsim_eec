4577ca82c8a5d7028cdd048f20eee9f9a96908c7
GeorgeYue1 <georgeyue08@gmail.com>
2025-11-10 09:43:56 -0600

    Added support for different CPU types

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -10,7 +10,6 @@
 using namespace std;
 
 static bool migrating = false;
-static unsigned active_machines = 16;
 
 struct MachineLoad {
     MachineId_t id;
@@ -23,147 +22,151 @@ struct MachineLoad {
 
 static vector<MachineLoad> cluster;
 static unordered_map<VMId_t, bool> vm_ready;
+static unordered_map<VMId_t, VMType_t> vm_types;  // Track VM types
 static map<TaskId_t, MachineId_t> task_map;  // Track which machine each task is on
+static unordered_map<MachineId_t, queue<TaskId_t>> pending_tasks;  // Tasks waiting for machine to power on
+
+// Helper function to check if VM type is compatible with CPU type
+static bool IsVMCompatibleWithCPU(VMType_t vm_type, CPUType_t cpu_type) {
+    switch(vm_type) {
+        case LINUX:
+        case LINUX_RT:
+            return true;  // Works with all CPU types
+        case WIN:
+            return (cpu_type == ARM || cpu_type == X86);
+        case AIX:
+            return (cpu_type == POWER);
+        default:
+            return false;
+    }
+}
+
+// Helper function to verify VM is ready to receive tasks
+static bool IsVMReady(VMId_t vm_id, MachineId_t machine_id) {
+    if(vm_ready.find(vm_id) == vm_ready.end() || !vm_ready[vm_id]) {
+        return false;
+    }
+    VMInfo_t vm_info = VM_GetInfo(vm_id);
+    return (vm_info.machine_id == machine_id);
+}
 
 void Scheduler::Init() {
-    // Find the parameters of the clusters
-    // Get the total number of machines
-    // For each machine:
-    //      Get the type of the machine
-    //      Get the memory of the machine
-    //      Get the number of CPUs
-    //      Get if there is a GPU or not
-    // 
-
-    // SimOutput("Scheduler::Init(): Total number of machines is " + to_string(Machine_GetTotal()), 3);
-    // SimOutput("Scheduler::Init(): Initializing scheduler", 1);
-    // for(unsigned i = 0; i < active_machines; i++)
-    //     vms.push_back(VM_Create(LINUX, X86));
-    // for(unsigned i = 0; i < active_machines; i++) {
-    //     machines.push_back(MachineId_t(i));
-    // }    
-    // for(unsigned i = 0; i < active_machines; i++) {
-    //     VM_Attach(vms[i], machines[i]);
-    // }
-
-    // bool dynamic = false;
-    // if(dynamic)
-    //     for(unsigned i = 0; i<4 ; i++)
-    //         for(unsigned j = 0; j < 8; j++)
-    //             Machine_SetCorePerformance(MachineId_t(0), j, P3);
-    // // Turn off the ARM machines
-    // for(unsigned i = 24; i < Machine_GetTotal(); i++)
-    //     Machine_SetState(MachineId_t(i), S5);
-
-    // SimOutput("Scheduler::Init(): VM ids are " + to_string(vms[0]) + " ahd " + to_string(vms[1]), 3);
-
     unsigned total = Machine_GetTotal();
     SimOutput("Scheduler::Init(): Total machines = " + to_string(total), 2);
     cluster.resize(total);
 
     for (unsigned i = 0; i < total; i++) {
         cluster[i].id = MachineId_t(i);
-        
-        // Get machine info to determine CPU type
         MachineInfo_t info = Machine_GetInfo(cluster[i].id);
-        CPUType_t machine_cpu = info.cpu;  // Get the machine's CPU type
         
-        // Create VM with LINUX (works with all CPU types) and matching CPU type
-        cluster[i].vm_id = VM_Create(LINUX, machine_cpu);
-        cluster[i].utilization = info.active_tasks;  // Initialize with current active tasks
-        // cluster[i].active = (i < active_machines);
+        cluster[i].vm_id = VM_Create(LINUX, info.cpu);
+        vm_types[cluster[i].vm_id] = LINUX;
+        cluster[i].utilization = info.active_tasks;
         cluster[i].active = true;
-
         cluster[i].memory_size = info.memory_size;
         cluster[i].memory_used = info.memory_used;
 
-        if (cluster[i].active) {
-            VM_Attach(cluster[i].vm_id, cluster[i].id);
-            vm_ready[cluster[i].vm_id] = true;  // VM is ready after attachment
-        } else {
-            Machine_SetState(cluster[i].id, S5);
-            vm_ready[cluster[i].vm_id] = false;  // VM not ready if machine is off
-        }
+        VM_Attach(cluster[i].vm_id, cluster[i].id);
+        vm_ready[cluster[i].vm_id] = true;
     }
 
     SimOutput("Scheduler::Init(): Greedy scheduler initialized.", 2);
 }
 
 void Scheduler::MigrationComplete(Time_t time, VMId_t vm_id) {
-    // Update your data structure. The VM now can receive new tasks
-    vm_ready[vm_id] = true;  // VM is ready after migration completes
+    vm_ready[vm_id] = true;
 }
 
 void Scheduler::NewTask(Time_t now, TaskId_t task_id) {
-    // Get the task parameters
-    //  IsGPUCapable(task_id);
-    //  GetMemory(task_id);
-    //  RequiredVMType(task_id);
-    //  RequiredSLA(task_id);
-    //  RequiredCPUType(task_id);
-    // Decide to attach the task to an existing VM, 
-    //      vm.AddTask(taskid, Priority_T priority); or
-    // Create a new VM, attach the VM to a machine
-    //      VM vm(type of the VM)
-    //      vm.Attach(machine_id);
-    //      vm.AddTask(taskid, Priority_t priority) or
-    // Turn on a machine, create a new VM, attach it to the VM, then add the task
-    //
-    // Turn on a machine, migrate an existing VM from a loaded machine....
-    //
-    // Other possibilities as desired
-
-    // Priority_t priority = (task_id == 0 || task_id == 64)? HIGH_PRIORITY : MID_PRIORITY;
-    // if(migrating) {
-    //     VM_AddTask(vms[0], task_id, priority);
-    // }
-    // else {
-    //     VM_AddTask(vms[task_id % active_machines], task_id, priority);
-    // }// Skeleton code, you need to change it according to your algorithm
-
+    VMType_t req_vm_type = RequiredVMType(task_id);
+    CPUType_t req_cpu_type = RequiredCPUType(task_id);
+    unsigned req_mem = GetTaskMemory(task_id);
+    
+    // Try to find a machine with matching CPU type and VM type
     for(auto &machine: cluster) {
         if(!machine.active) continue;
         
-        // Check if VM is ready (attached and not migrating)
-        if(!vm_ready[machine.vm_id]) continue;
-        
-        // Get current machine state
         MachineInfo_t info = Machine_GetInfo(machine.id);
+        if(info.cpu != req_cpu_type || info.s_state != S0) continue;
+        if(vm_ready.find(machine.vm_id) == vm_ready.end() || !vm_ready[machine.vm_id]) continue;
+        if(vm_types.find(machine.vm_id) == vm_types.end() || vm_types[machine.vm_id] != req_vm_type) continue;
+        
         unsigned active_tasks = info.active_tasks;
         unsigned num_cpus = info.num_cpus;
         unsigned avail_mem = info.memory_size - info.memory_used;
-        unsigned req_mem = GetTaskMemory(task_id);
         
-        // Check if machine has available CPUs and memory
-        if(active_tasks < num_cpus && req_mem <= avail_mem) {
-            // Update utilization based on actual CPU usage
-            machine.utilization = active_tasks + 1;  // Will be +1 after task is added
-            machine.memory_used += req_mem;
+        if(active_tasks < num_cpus && req_mem <= avail_mem && IsVMReady(machine.vm_id, machine.id)) {
             VM_AddTask(machine.vm_id, task_id, MID_PRIORITY);
-            task_map[task_id] = machine.id;  // Track which machine this task is on
+            machine.utilization = active_tasks + 1;
+            machine.memory_used += req_mem;
+            task_map[task_id] = machine.id;
             return;
         }
     }
+
+    // Try to create a new VM on a matching active machine
+    for(auto &machine: cluster) {
+        if(!machine.active) continue;
+        
+        MachineInfo_t info = Machine_GetInfo(machine.id);
+        if(info.cpu != req_cpu_type || info.s_state != S0) continue;
+        if(!IsVMCompatibleWithCPU(req_vm_type, info.cpu)) continue;
+        
+        unsigned active_tasks = info.active_tasks;
+        unsigned num_cpus = info.num_cpus;
+        unsigned avail_mem = info.memory_size - info.memory_used;
+        
+        if(active_tasks < num_cpus && req_mem <= avail_mem) {
+            if(vm_ready[machine.vm_id]) {
+                VM_Shutdown(machine.vm_id);
+                vm_ready[machine.vm_id] = false;
+            }
+            
+            VMId_t new_vm = VM_Create(req_vm_type, req_cpu_type);
+            vm_types[new_vm] = req_vm_type;
+            machine.vm_id = new_vm;
+            VM_Attach(new_vm, machine.id);
+            
+            VMInfo_t vm_info = VM_GetInfo(new_vm);
+            if(vm_info.machine_id == machine.id) {
+                vm_ready[new_vm] = true;
+                machine.utilization = active_tasks + 1;
+                machine.memory_used += req_mem;
+                VM_AddTask(new_vm, task_id, MID_PRIORITY);
+                task_map[task_id] = machine.id;
+                return;
+            }
+            vm_ready[new_vm] = false;
+        }
+    }
+    
+    // Try to power on an inactive machine with matching CPU type
+    for(auto &machine: cluster) {
+        if(machine.active) continue;
+        
+        MachineInfo_t info = Machine_GetInfo(machine.id);
+        if(info.cpu != req_cpu_type || !IsVMCompatibleWithCPU(req_vm_type, info.cpu)) continue;
+        
+        Machine_SetState(machine.id, S0);
+        VMId_t new_vm = VM_Create(req_vm_type, req_cpu_type);
+        vm_types[new_vm] = req_vm_type;
+        machine.vm_id = new_vm;
+        machine.active = true;
+        pending_tasks[machine.id].push(task_id);
+        return;
+    }
+    
+    SimOutput("SLA Violation: No available capacity for task " + to_string(task_id), 0);
 }
 
 void Scheduler::PeriodicCheck(Time_t now) {
-    // This method should be called from SchedulerCheck()
-    // SchedulerCheck is called periodically by the simulator to allow you to monitor, make decisions, adjustments, etc.
-    // Unlike the other invocations of the scheduler, this one doesn't report any specific event
-    // Recommendation: Take advantage of this function to do some monitoring and adjustments as necessary
+    // Periodic optimization can be added here if needed
 }
 
 void Scheduler::Shutdown(Time_t time) {
-    // Do your final reporting and bookkeeping here.
-    // Report about the total energy consumed
-    // Report about the SLA compliance
-    // Shutdown everything to be tidy :-)
     for(auto &machine: cluster) {
-        if(machine.active) {
-            // Only shut down VM if it's actually attached/ready
-            if(vm_ready[machine.vm_id]) {
-                VM_Shutdown(machine.vm_id);
-            }
+        if(machine.active && vm_ready[machine.vm_id]) {
+            VM_Shutdown(machine.vm_id);
             Machine_SetState(machine.id, S5);
         }
     }
@@ -172,83 +175,61 @@ void Scheduler::Shutdown(Time_t time) {
 }
 
 void Scheduler::TaskComplete(Time_t now, TaskId_t task_id) {
-    // Do any bookkeeping necessary for the data structures
-    // Decide if a machine is to be turned off, slowed down, or VMs to be migrated according to your policy
-    // This is an opportunity to make any adjustments to optimize performance/energy
     SimOutput("Scheduler::TaskComplete(): Task " + to_string(task_id) + " is complete at " + to_string(now), 4);
     
-    // Find which machine this task was on
     if(task_map.find(task_id) == task_map.end()) return;
-    MachineId_t host_machine = task_map[task_id];
     task_map.erase(task_id);
     
-    // Update utilization and memory usage of all machines
+    // Update utilization and memory usage
     for(auto &machine: cluster) {
         if(!machine.active) continue;
         MachineInfo_t info = Machine_GetInfo(machine.id);
-        machine.utilization = info.active_tasks;  // Update based on actual active tasks
+        machine.utilization = info.active_tasks;
         machine.memory_used = info.memory_used;
     }
     
-    // Try to consolidate workloads (skip if migration ongoing)
+    // Consolidate workloads: migrate from lightly loaded to more utilized machines
     if(!migrating) {
-        // Sort machines by ascending utilization (only active machines)
         vector<pair<unsigned, MachineId_t>> machines_by_util;
         for(auto &m: cluster) {
             if(!m.active) continue;
             MachineInfo_t info = Machine_GetInfo(m.id);
-            // Only consider machines that are powered on (S0)
             if(info.s_state == S0) {
                 machines_by_util.push_back({m.utilization, m.id});
             }
         }
         sort(machines_by_util.begin(), machines_by_util.end());
         
-        // Migrate VMs from lightly loaded machines to more utilized ones
-        // Check if u_src + u_dst < num_cores of destination
-        bool migration_found = false;
-        for(size_t i = 0; i < machines_by_util.size() && !migration_found; i++) {
+        for(size_t i = 0; i < machines_by_util.size(); i++) {
             MachineId_t src_id = machines_by_util[i].second;
             unsigned u_src = machines_by_util[i].first;
             
-            // Try destinations with higher utilization
-            for(size_t j = i + 1; j < machines_by_util.size() && !migration_found; j++) {
+            for(size_t j = i + 1; j < machines_by_util.size(); j++) {
                 MachineId_t dst_id = machines_by_util[j].second;
                 unsigned u_dst = machines_by_util[j].first;
-                
-                // Get destination machine info to check num_cores
                 MachineInfo_t dst_info = Machine_GetInfo(dst_id);
-                unsigned dst_num_cores = dst_info.num_cpus;
                 
-                // Check if migration is feasible: u_src + u_dst < num_cores of destination
-                // Destination must be active and in S0 state (already filtered above)
-                if(u_src + u_dst < dst_num_cores && vm_ready[cluster[src_id].vm_id]) {
+                if(u_src + u_dst < dst_info.num_cpus && vm_ready[cluster[src_id].vm_id]) {
                     VM_Migrate(cluster[src_id].vm_id, dst_id);
                     migrating = true;
-                    vm_ready[cluster[src_id].vm_id] = false;  // VM not ready during migration
-                    SimOutput("TaskComplete(): Migrating VM " + to_string(cluster[src_id].vm_id) +
-                              " from machine " + to_string(src_id) + " (u=" + to_string(u_src) + 
-                              ") -> machine " + to_string(dst_id) + " (u=" + to_string(u_dst) + 
-                              "), total=" + to_string(u_src + u_dst) + " < " + to_string(dst_num_cores) + " cores", 2);
-                    migration_found = true;
+                    vm_ready[cluster[src_id].vm_id] = false;
+                    return;
                 }
             }
         }
     }
     
-    // Power down idle machines: if no running tasks -> shut down VM + set state to S5
+    // Power down idle machines
     for(auto &machine: cluster) {
         if(!machine.active) continue;
         MachineInfo_t info = Machine_GetInfo(machine.id);
         if(info.active_tasks == 0) {
-            // Only shut down VM if it's actually attached/ready
             if(vm_ready[machine.vm_id]) {
                 VM_Shutdown(machine.vm_id);
             }
             Machine_SetState(machine.id, S5);
             machine.active = false;
             vm_ready[machine.vm_id] = false;
-            SimOutput("TaskComplete(): Machine " + to_string(machine.id) + " powered off (idle)", 2);
         }
     }
 }
@@ -309,21 +290,42 @@ void SLAWarning(Time_t time, TaskId_t task_id) {
 }
 
 void StateChangeComplete(Time_t time, MachineId_t machine_id) {
-    // Called in response to an earlier request to change the state of a machine
     SimOutput("StateChangeComplete(): Machine " + to_string(machine_id) + " state change complete at " + to_string(time), 3);
     
-    // Find the machine and mark its VM as ready
     for(auto &machine: cluster) {
         if(machine.id == machine_id) {
             machine.active = true;
             
-            // Check if VM is already attached to this machine
             VMInfo_t vm_info = VM_GetInfo(machine.vm_id);
             if(vm_info.machine_id != machine.id) {
-                // VM is not attached to this machine, so attach it
                 VM_Attach(machine.vm_id, machine.id);
+                vm_info = VM_GetInfo(machine.vm_id);
             }
-            vm_ready[machine.vm_id] = true;  // VM is ready after attachment
+            vm_ready[machine.vm_id] = (vm_info.machine_id == machine.id);
+            
+            // Process pending tasks
+            while(!pending_tasks[machine_id].empty()) {
+                TaskId_t task_id = pending_tasks[machine_id].front();
+                pending_tasks[machine_id].pop();
+                
+                if(!IsVMReady(machine.vm_id, machine.id)) {
+                    pending_tasks[machine_id].push(task_id);
+                    break;
+                }
+                
+                unsigned req_mem = GetTaskMemory(task_id);
+                MachineInfo_t info = Machine_GetInfo(machine.id);
+                
+                if(info.active_tasks < info.num_cpus && req_mem <= (info.memory_size - info.memory_used)) {
+                    VM_AddTask(machine.vm_id, task_id, MID_PRIORITY);
+                    task_map[task_id] = machine.id;
+                    machine.utilization = info.active_tasks + 1;
+                    machine.memory_used += req_mem;
+                } else {
+                    pending_tasks[machine_id].push(task_id);
+                    break;
+                }
+            }
             break;
         }
     }
